91a88a7c794ddce4e2b7509f53654d077bd5f1b9
clement <clementwanjau@gmail.com>
2024-04-24 08:23:38 +0300

    Add some error variants.

--- src/error.rs
+++ src/error.rs
@@ -6,4 +6,18 @@ pub enum Error {
     ArchNotImplemented(String),
     #[error("{0}")]
     FuncNotImplemented(String),
+    #[error("No valid free memory was found of size: {0}")]
+    NoValidFreeMemoryFound(i32),
+    #[error("No map found at address ({0})!")]
+    MapNotFound(i32),
+    #[error("Segmentation fault at address ({0})! {1}")]
+    SegmentationViolation(i32, String),
+    #[error("Invalid state: {0}")]
+    InvalidState(String),
+    #[error("Invalid register name: {0}")]
+    InvalidRegisterName(String),
+    #[error("Invalid calling convention: {0}")]
+    UnknownCallingConvention(String),
+    #[error("Error: {0}")]
+    Generic(String)
 }
